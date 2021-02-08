bc1452b88c50f2d7005154c49aca03e64e7cf714
Kody Samaroo <github email address>
2021-02-08 11:25:56 -0500

    Add toy info to cards and display

--- src/index.js
+++ src/index.js
@@ -12,4 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch(`http://localhost:3000/toys`)
+    .then(res => res.json())
+    .then(toys => toys.forEach(renderToys))
+
+  function renderToys(toy){
+    console.log(toy)
+    const card = document.createElement("div")
+    card.className = "card"
+    const collection = document.querySelector("#toy-collection")
+    collection.append(card)
+
+    card.innerHTML = `<h2>${toy.name}</h2><img src=${toy.image} class="toy-avatar" width="250" height="250"><p>${toy.likes} Likes</p><button class="like-btn">Like </button>`
+
+  }
 });
