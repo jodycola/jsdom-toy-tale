3f2e449efc51d0c64f9bcaa5b1d0ee8c5a6a0411
menachem margolis <margolismendel5728@gmail.com>
2021-02-08 12:01:39 -0500

    trying to create  new toy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".add-toy-form")
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -18,13 +19,45 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(toys => toys.forEach(renderToys))
 
   function renderToys(toy){
-    console.log(toy)
     const card = document.createElement("div")
     card.className = "card"
     const collection = document.querySelector("#toy-collection")
     collection.append(card)
 
     card.innerHTML = `<h2>${toy.name}</h2><img src=${toy.image} class="toy-avatar" width="250" height="250"><p>${toy.likes} Likes</p><button class="like-btn">Like </button>`
-
+    
   }
+  
+  form.addEventListener('submit',createNewToy)
+  
+  // function createNewToy(e){
+  //   e.preventDefault()
+    
+  //   const toyData = {name: e.target[0].value, image: e.target[1].value}
+
+  //   fetch(`http://localhost:3000/toys`,{
+  //     method: 'POST',
+  //     headers:{
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(toyData),
+  //   })
+  //   .then(response => response.json())
+  //   .then(input => console.log(input))
+  // }
+  
 });
+
+// const data = { username: 'example' };
+
+// fetch('https://example.com/profile', {
+//   method: 'POST', // or 'PUT'
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(data),
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Success:', data);
+// })
